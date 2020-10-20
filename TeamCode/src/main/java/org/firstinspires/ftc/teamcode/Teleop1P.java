d9446f1ac8efd4d9d1db44eb3394958f20e955c6
LearnTheABCs <nicholasjtchang@gmail.com>
2020-10-19 20:59:18 -0700

    Initial 1P driving code added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop1P.java
@@ -0,0 +1,240 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.media.MediaPlayer;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+
+@TeleOp(name="Teleop 1P", group="Linear Opmode")
+public class Teleop1P extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private double leftFrontPower;
+    private double leftBackPower;
+    private double rightFrontPower;
+    private double rightBackPower;
+    private double drive; //testing
+    private double strafe;
+    private double rotate;
+    private boolean button_a;
+    private boolean button_b;
+    private boolean button_du;
+    private boolean button_dd;
+    private boolean bumper_left;
+    private boolean bumper_right;
+    private boolean button_dl;
+    private boolean button_dr;
+    private boolean targetVisible = false;
+    private OpenGLMatrix lastLocation = null;
+
+    private static final double INCREMENT = 0.03;
+    private static final int CYCLE_MS = 50;
+    private static final double MAX_POS = 1.0;
+    private static final double MIN_POS = 0.0;
+    private double position = (MAX_POS - MIN_POS) / 2;
+
+    private static final double BINCREMENT = 0.05;
+    private static final double BBMAX_POS = 0.65;
+    private static final double BBMIN_POS = 0.0;
+    private static final double BMAX_POS = 1;
+    private static final double BMIN_POS = 0.35;
+    private double bposition = BMAX_POS;
+    private double bbposition = BBMIN_POS;
+
+    private static final double CINCREMENT = 0.06;
+    private static final double CMAX_POS = 1.0;
+    private static final double CMIN_POS = 0.0;
+    private double cposition = (0.4);
+
+    private static final double DINCREMENT = 0.03;
+    private static final double DMAX_POS = 1.0;
+    private static final double DMIN_POS = 0.0;
+    private double dposition = (DMAX_POS - DMIN_POS) / 2;
+    private double frontIsSkystone;
+    private double backIsSkystone;
+    int front_red, front_green, front_blue, back_red, back_green, back_blue;
+
+    VoyagerBot robot = new VoyagerBot();
+    //Detecting detector = null;
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        //detector = new Detecting(this, robot);
+        robot.resetEncoders();
+        telemetry.addData("Status", "Ready");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+        while(opModeIsActive()) {
+            drive = -gamepad1.left_stick_y; //forward and backward
+            strafe = 0.90 * gamepad1.right_stick_x; // side to side and diagonal
+            rotate = 0.85 * gamepad1.left_stick_x; // rotate in place
+            button_a = gamepad1.a;
+            button_b = gamepad1.b;
+            button_du = gamepad1.dpad_up;
+            button_dd = gamepad1.dpad_down;
+            button_dl = gamepad1.dpad_left;
+            button_dr = gamepad1.dpad_right;
+            bumper_left = gamepad1.left_bumper;
+            bumper_right = gamepad1.right_bumper;
+            if(gamepad1.right_stick_button) {
+                rotate = 0.5 * rotate;
+                drive = 0.5 * drive;
+                strafe = 0.5 * strafe;
+            }
+            /*
+            if(bumper_left) {
+                robot.lift.setPower(1.0);
+            }
+            if(bumper_right) {
+                robot.lift.setPower(-1);
+            }
+            if(!bumper_left && !bumper_right) {
+                robot.lift.setPower(0);
+            }
+            if(button_a) {
+                position += INCREMENT;
+                if(position >= MAX_POS) {
+                    position = MAX_POS;
+                }
+            } else if(button_b) {
+                position -= INCREMENT;
+                if (position <= MIN_POS) {
+                    position = MIN_POS;
+                }
+            }
+            if(button_dr) {
+                cposition += CINCREMENT;
+                if(cposition >= CMAX_POS) {
+                    cposition = CMAX_POS;
+                }
+            } else if(button_dl) {
+                cposition -= CINCREMENT;
+                if (cposition <= CMIN_POS) {
+                    cposition = CMIN_POS;
+                }
+            }
+            if(button_dd) {
+                bposition += BINCREMENT;
+                bbposition -= BINCREMENT;
+                if(bposition >= BMAX_POS) {
+                    bposition = BMAX_POS;
+                }
+                if (bbposition <= BBMIN_POS) {
+                    bbposition = BBMIN_POS;
+                }
+            } else if(button_du) {
+                bposition -= BINCREMENT;
+                bbposition += BINCREMENT;
+                if (bposition <= BMIN_POS) {
+                    bposition = BMIN_POS;
+                }
+                if (bbposition >= BBMAX_POS) {
+                    bbposition = BBMAX_POS;
+                }
+            }
+            if(gamepad2.dpad_down) {
+                robot.extension.setPower(0.3);
+            }
+            if(gamepad2.dpad_up) {
+                robot.extension.setPower(-0.3);
+            }
+            if(!gamepad2.dpad_up && !gamepad2.dpad_down) {
+                robot.extension.setPower(0);
+            }
+            /*if(gamepad1.y) {
+                robot.yeeter.setPower(0.7);
+            } else {
+                robot.yeeter.setPower(0);
+            }
+            if(gamepad1.x) {
+                robot.yeeter.setPower(-0.7);
+            } else {
+                robot.yeeter.setPower(0);
+            }*/ // TODO: yeeter is gone!!!!!!!!!!
+
+            /*
+            if(gamepad1.x) {
+                dposition += DINCREMENT;
+                if(dposition >= DMAX_POS) {
+                    dposition = DMAX_POS;
+                }
+            } else if(gamepad1.y) {
+                dposition -= DINCREMENT;
+                if (dposition <= DMIN_POS) {
+                    dposition = DMIN_POS;
+                }
+            }
+            */
+
+            /*if (((VuforiaTrackableDefaultListener)detector.stoneTarget.getListener()).isVisible()) {
+                telemetry.addData("Visible Target", detector.stoneTarget.getName());
+                targetVisible = true;
+                OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)detector.stoneTarget.getListener()).getUpdatedRobotLocation();
+                if (robotLocationTransform != null) {
+                    lastLocation = robotLocationTransform;
+                }
+                break;
+            }
+            if(targetVisible) {
+                VectorF translation = lastLocation.getTranslation();
+                telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                        translation.get(0) / detector.mmPerInch, translation.get(1) / detector.mmPerInch, translation.get(2) / detector.mmPerInch);
+                Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+                telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
+            } else {
+                telemetry.addData("Visible Target", "none");
+            }*/
+
+            /*
+            front_red = robot.color_front.red();
+            front_green = robot.color_front.green();
+            front_blue = robot.color_front.blue();
+            back_red = robot.color_back.red();
+            back_green = robot.color_back.green();
+            back_blue = robot.color_back.blue();
+            frontIsSkystone = (front_red + front_green) / front_blue;
+            backIsSkystone = (back_red + back_green) / back_blue;
+            telemetry.addData("Front Skystone", frontIsSkystone);
+            telemetry.addData("Back Skystone", backIsSkystone);
+            telemetry.update();
+             */
+
+            /* * * * * * * * * * * *
+             * Left stick:
+             * - up and down moves forwards and backwards
+             * - left and right rotates the robot in place
+             * Right stick:
+             * - strafing side to side
+             * apparently you can face forward while moving in a circle
+             * by strafing and rotating at the same time
+             */
+            leftFrontPower = drive + strafe + rotate;
+            leftBackPower = drive - strafe + rotate;
+            rightFrontPower = drive - strafe - rotate;
+            rightBackPower = drive + strafe - rotate;
+
+            robot.leftFront.setPower(leftFrontPower);
+            robot.leftBack.setPower(leftBackPower);
+            robot.rightFront.setPower(rightFrontPower);
+            robot.rightBack.setPower(rightBackPower);
+            robot.back.setPosition(bposition);
+            robot.back2.setPosition(bbposition);
+            robot.claw.setPosition(position);
+            robot.skystone.setPosition(cposition);
+            robot.gripper.setPosition(dposition);
+            sleep(CYCLE_MS);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoyagerBot.java
@@ -0,0 +1,119 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class VoyagerBot {
+    public DcMotor leftFront = null;
+    public DcMotor leftBack = null;
+    public DcMotor rightFront = null;
+    public DcMotor rightBack = null;
+    public Servo claw = null;
+    public Servo back = null;
+    public Servo skystone = null;
+    public DcMotor lift = null;
+    public DcMotor yeeter = null;
+    HardwareMap hwMap = null;
+    public CRServo extension = null;
+    public Servo back2 = null;
+    public Servo gripper = null;
+    public ColorSensor color_front = null;
+    public ColorSensor color_back = null;
+    public RevBlinkinLedDriver underglow = null;
+    public Rev2mDistanceSensor distance = null;
+    BNO055IMU imu = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    static final double COUNTS_PER_MOTOR_REV = 537.6;
+    static final double DRIVE_GEAR_REDUCTION = 19.2;
+    static final double WHEEL_RADIUS_INCHES = 10 / 2.54;
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_RADIUS_INCHES * Math.PI);
+    public VoyagerBot() {
+
+    }
+    public void resetEncoders() {
+        this.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void newReset() {
+        this.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+        leftFront = hwMap.get(DcMotor.class, "left_front");
+        leftBack = hwMap.get(DcMotor.class, "left_back");
+        rightFront = hwMap.get(DcMotor.class, "right_front");
+        rightBack = hwMap.get(DcMotor.class, "right_back");
+        yeeter = hwMap.get(DcMotor.class, "yeeter");
+        claw = hwMap.get(Servo.class, "claw");
+        back = hwMap.get(Servo.class, "back");
+        gripper = hwMap.get(Servo.class, "gripper");
+        lift = hwMap.get(DcMotor.class, "lift_motor");
+        extension = hwMap.get(CRServo.class, "extension");
+        //extension = hwMap.get(Servo.class, "extension");
+        back2 = hwMap.get(Servo.class, "back2");
+        skystone = hwMap.get(Servo.class, "stone");
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        color_front = hwMap.get(ColorSensor.class, "color_front");
+        color_back = hwMap.get(ColorSensor.class, "color_back");
+        color_front.enableLed(false);
+        color_back.enableLed(false);
+        distance = hwMap.get(Rev2mDistanceSensor.class, "distance");
+        underglow = hwMap.get(RevBlinkinLedDriver.class, "underglow");
+        underglow.setPattern(RevBlinkinLedDriver.BlinkinPattern.LIGHT_CHASE_RED);
+
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        yeeter.setPower(0);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        claw.setPosition(0);
+        back.setPosition(0.35);
+        back2.setPosition(0.65);
+        extension.setPower(0);
+        extension.setPower(0);
+        gripper.setPosition(0.1);
+        skystone.setPosition(0.4);
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+}
