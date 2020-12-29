ac5683cc68bcff40ee8e443e4134878d3e0cda90
LearnTheABCs <nicholasjtchang@gmail.com>
2020-12-28 20:25:58 -0800

    created test autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="Left: Buildplate Wall")
+@Autonomous(name="Drive Forward")
 public class AutonomousTest extends LinearOpMode {
     Driving driver = new Driving(this);
     @Override
